52a4cee4bfed1e13e2ea830f8df63a93ad83d526
thealytaproject <the.alyta.project@gmail.com>
2015-12-02 13:34:08 -0500

    Countdown

--- countdown/src/countdown.js
+++ countdown/src/countdown.js
@@ -16,6 +16,17 @@ var defaultOptions = {
   }
 };
 
+function decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
+
 /**
  * Countdown constructor
  * @param {HTMLElement} el      DOM node of the countdown
